f37b5f912fa61e0b409c8284a767fa2144682709
Paul Grau <paul@graycoding.com>
2017-07-11 13:41:43 +0900

    Add some text to start page

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <button class="ui basic green button" @click="$router.push('signUp')">Start</button>
+    <p class="intro-text">Browse, check and speak!</p>
+    <ul class="intro-text check-list">
+<li>Browse politician’s 
+    promises that relate to YOU</li>
+<li>Check the progress of 
+    promises</li>
+<li>Make your voice heard</li>
+</ul>
+
+    <button class="ui basic big green button" @click="$router.push('signUp')">Get Started</button>
   </div>
 </template>
 
@@ -11,6 +20,19 @@ export default {
 </script>
 
 <style>
-
+.intro-text {
+  font-size: 2em;
+  line-height: 1.5;
+}
+.check-list {
+  list-style-type: none;
+  text-align: left;
+}
+.check-list li {
+  text-indent: -1.1em;
+}
+.check-list li:before {
+  content: '✓ ';
+}
 </style>
 
