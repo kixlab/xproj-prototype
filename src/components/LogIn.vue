e5341de4f541133e6658d5c81a242f188fc27a81
Hyunwoo Kim <khw0726@kaist.ac.kr>
2017-10-16 13:08:11 +0900

    login working piece

---
+++ src/components/LogIn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="ui header">Log in</h1>
+    <div class="ui form">
+      <div class="field">
+        <input placeholder="E-mail" type="email" v-model="email">
+      </div>
+      <div class="field">
+        <input placeholder="Password" type="password" v-model="password">
+      </div>
+      <button class="ui button" type="submit" @submit.prevent @click="onSubmit">Log in</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'logIn',
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit: function () {
+      console.log('onSubmit!')
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
